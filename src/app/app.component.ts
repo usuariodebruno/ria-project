7bbca6d6068e08bf32c17f30865c781730a22fd1
Bruno Almeida <usuariodebruno@gmail.com>
2024-07-12 14:24:55 -0300

    feat: adiciona pastas (#Lab 12)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent],
+  imports: [RouterOutlet, FirstComponentComponent, FormsModule, MatSlideToggleModule, MatInputModule, MatFormFieldModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ria-project';
 }
